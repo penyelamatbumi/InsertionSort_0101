5bf34701bd3d9ed3e4677313287c12269792f2c0
penyelamatbumi <inihanip7@gmail.com>
2026-03-12 12:05:20 +0800

    menambahkan library

--- file1.cpp
+++ file1.cpp
@@ -49,3 +49,22 @@ void insertionsort()
         }
     }
 }
+
+void display(){
+    cout << endl;
+    cout << "\n================";
+    cout <<"\nElement Array" << endl;
+    cout << "\n================";
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
+int main(){
+    input();
+    insertionsort();
+    display();
+    system("pause");
+}
