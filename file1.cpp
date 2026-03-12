0271c57dd111cfbaa709f08db9249864750b5dda
penyelamatbumi <inihanip7@gmail.com>
2026-03-12 12:06:11 +0800

    menambahkan insert

--- file1.cpp
+++ file1.cpp
@@ -68,3 +68,32 @@ int main(){
     display();
     system("pause");
 }
+
+#include <iostream>
+using namespace std;
+
+int arr[20];        //membuat array dengan panjang data 20
+int n;              //membuat variable inputan n
+
+void input (){ //procedure input
+    while (true){
+        cout << "Masukkan Jumlah Data Pada Array : "; 
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "==============================================";
+    cout << "==========Masukkan Element Array==============";
+    cout << "==============================================";
+
+    for (int i = 0; i < n; i++ ){
+        cout << "data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
